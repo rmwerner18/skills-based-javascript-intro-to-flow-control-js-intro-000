6111caefb822e75165d0155db8e941cb94d07d01
Ryan Werner <rmwerner@umich.edu>
2020-05-10 23:08:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,7 +11,7 @@ else {return `You are not a teenager`}
 function ageChecker(age) {if (age <= 19 && age >= 13)
 {return `You are a teenager!`}
 else if (age <= 12) {return `You are a kid`}
-else {`You are a grownup`}
+else {return `You are a grownup`}
 }
 
 function ternaryTeenager(age) {return age >= 13 && age <= 19 ?
@@ -20,19 +20,19 @@ function ternaryTeenager(age) {return age >= 13 && age <= 19 ?
 
 function switchAge(age) {
 switch(age) {
- case 13 : console.log("You are a teenager");
+ case 13 : console.log('You are a teenager');
 break;
- case 14 : console.log("You are a teenager");
+ case 14 : console.log('You are a teenager');
 break;
- case 15 : console.log("You are a teenager");
+ case 15 : console.log('You are a teenager');
 break;
- case 16 : console.log("You are a teenager");
+ case 16 : console.log('You are a teenager');
 break;
- case 17 : console.log("You are a teenager");
+ case 17 : console.log('You are a teenager');
 break;
- case 18 : console.log("You are a teenager");
+ case 18 : console.log('You are a teenager');
 break;
- case 19 : console.log("You are a teenager");
+ case 19 : console.log('You are a teenager');
 break;
-    default : console.log("You have an age");}
+    default : console.log('You have an age');}
 }
